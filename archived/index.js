1c3ea7ff120e86415a88d3a9cbdbd61ed24b8f11
Kelsey Dawes <kdawes@gmail.com>
2015-10-19 22:43:04 -0600

    archive some cruft

---
+++ archived/index.js
@@ -0,0 +1,35 @@
+// This is the POC for piping an xml / OPML file through a transform stream
+// and pumping out a JSON repr.
+
+var parser = require('xml2json')
+
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+
+var data = ''
+
+process.stdin.on('data', function (chunk) {
+  data += chunk
+})
+
+process.stdin.on('end', function () {
+  var obj = parser.toJson(data)
+  console.log(obj)
+// inspectIt(obj, 1)
+})
+
+function tabs ( n ) {
+  var ntmp = n || 1
+  return Array(ntmp).join('\t')
+}
+
+function inspectIt ( obj, depth ) {
+  var o = JSON.parse(obj)
+  for ( var i in o) {
+    var type = typeof (o[i])
+    console.log(tabs(depth) + i + ' : ' + type)
+    if ( type === 'object') {
+      inspectIt(JSON.stringify(o[i]), (depth + 1))
+    }
+  }
+}
